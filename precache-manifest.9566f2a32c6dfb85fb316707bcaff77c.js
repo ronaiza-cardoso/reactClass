64df949d0cf113010b6817002e92baa0a0cb2173
Ronaiza Cardoso <ronaizacard@gmail.com>
2018-11-03 17:10:16 +0000

    Updates

---
+++ precache-manifest.9566f2a32c6dfb85fb316707bcaff77c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c3051cccc94a49fb8ed",
+    "url": "/reactClass/static/js/runtime~main.4c3051cc.js"
+  },
+  {
+    "revision": "d6efa7e375db465907f4",
+    "url": "/reactClass/static/js/main.d6efa7e3.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/reactClass/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "d6efa7e375db465907f4",
+    "url": "/reactClass/static/css/main.f0d1a87c.chunk.css"
+  },
+  {
+    "revision": "d23845109ff3264c5ab59a326df5df1a",
+    "url": "/reactClass/index.html"
+  }
+];
